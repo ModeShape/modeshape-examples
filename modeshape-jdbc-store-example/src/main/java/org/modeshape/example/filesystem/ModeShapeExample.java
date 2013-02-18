59648dceece54183cb921a2b299f8bf52dae7c38
Randall Hauch <rhauch@gmail.com>
2013-02-18 11:33:59 -0600

    Added an example that stores regular content in a local H2 database via JDBC

---
+++ modeshape-jdbc-store-example/src/main/java/org/modeshape/example/filesystem/ModeShapeExample.java
@@ -0,0 +1,91 @@
+package org.modeshape.example.filesystem;
+
+import java.io.ByteArrayInputStream;
+import java.net.URL;
+import java.util.Random;
+import javax.jcr.*;
+import org.modeshape.common.collection.Problems;
+import org.modeshape.jcr.ModeShapeEngine;
+import org.modeshape.jcr.RepositoryConfiguration;
+import org.modeshape.jcr.value.BinaryValue;
+
+public class ModeShapeExample {
+
+    public static void main( String[] argv ) {
+
+        Random rnd = new Random();
+
+        // Create and start the engine ...
+        ModeShapeEngine engine = new ModeShapeEngine();
+        engine.start();
+
+        // Load the configuration for a repository via the classloader (can also use path to a file)...
+        Repository repository = null;
+        String repositoryName = null;
+        try {
+            URL url = ModeShapeExample.class.getClassLoader().getResource("my-repository-config.json");
+            RepositoryConfiguration config = RepositoryConfiguration.read(url);
+
+            // We could change the name of the repository programmatically ...
+            // config = config.withName("Some Other Repository");
+
+            // Verify the configuration for the repository ...
+            Problems problems = config.validate();
+            if (problems.hasErrors()) {
+                System.err.println("Problems starting the engine.");
+                System.err.println(problems);
+                System.exit(-1);
+            }
+
+            // Deploy the repository ...
+            repository = engine.deploy(config);
+            repositoryName = config.getName();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            System.exit(-1);
+            return;
+        }
+
+        Session session = null;
+        try {
+            // Get the repository
+            repository = engine.getRepository(repositoryName);
+
+            // Create a session ...
+            session = repository.login("default");
+
+            // Get the root node ...
+            Node root = session.getRootNode();
+            assert root != null;
+
+            System.out.println("Found the root node in the \"" + session.getWorkspace().getName() + "\" workspace");
+
+            Node n = root.addNode("Node" + rnd.nextInt());
+
+            n.setProperty("key", "value");
+            n.setProperty("content", session.getValueFactory().createBinary(new ByteArrayInputStream(new byte[1000])));
+
+            session.save();
+
+            System.out.println("Added one node under root");
+            System.out.println("+ Root childs");
+
+            NodeIterator it = root.getNodes();
+            while (it.hasNext()) {
+                System.out.println("+---> " + it.nextNode().getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) session.logout();
+            System.out.println("Shutting down engine ...");
+            try {
+                engine.shutdown().get();
+                System.out.println("Success!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
